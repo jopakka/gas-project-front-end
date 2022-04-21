0857bce9eb92a4d44c3cd97e976dbbfa41ec91b5
jopakka <jopakkamies@gmail.com>
2022-04-21 12:36:07 +0300

    all station markers working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import './App.css';
 import Map from './views/Map';
-import {stationsByBounds} from './utils/queries';
-import {useQuery} from '@apollo/client';
 
 const App = () => {
-  const {loading, error, data, refetch} = useQuery(
-      stationsByBounds);
-  console.log("loading", loading)
-  console.log("error", error)
-  console.log("data", data)
   return (
       <div className="App">
-        <Map moveEndedAction={async (b) => {
-          const bounds = {
-            s: b._southWest.lat,
-            w: b._southWest.lng,
-            n: b._northEast.lat,
-            e: b._northEast.lng,
-          }
-          console.log("refetch on move")
-          await refetch({bounds})
-        }} stations={data && data.stationsByBounds}/>
+        <Map showAllStation />
       </div>
   );
 };
--- src/views/Map.js
+++ src/views/Map.js
@@ -1,12 +1,9 @@
-import {
-  MapContainer,
-  Marker,
-  TileLayer,
-  useMapEvent, ZoomControl,
-} from 'react-leaflet';
+import {MapContainer, Marker, TileLayer, ZoomControl} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import StationMarker from '../components/StationMarker';
+import {useCallback, useEffect, useMemo, useState} from 'react';
+import {useLazyQuery} from '@apollo/client';
+import {stationsByBounds} from '../utils/queries';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -18,28 +15,73 @@ L.Icon.Default.mergeOptions({
 
 const position = [60.1689856, 24.9358008];
 
-const MapEvents = ({moveEndedAction}) => {
-  const map = useMapEvent('moveend', () => {
-    if(moveEndedAction) {
-      console.log("moveEnded")
-      moveEndedAction(map.getBounds())
-    }
-  })
-  return null
-}
+const mapBounds = (b) => {
+  return {
+    s: b._southWest.lat,
+    w: b._southWest.lng,
+    n: b._northEast.lat,
+    e: b._northEast.lng,
+  };
+};
 
-const Map = ({moveEndedAction, stations, width = "100vw", height = "100vh", center = position}) => {
+const DisplayStations = ({map}) => {
+  const [getStations, {data}] = useLazyQuery(stationsByBounds);
+  const [stations, setStations] = useState([]);
+
+  const moveEnd = useCallback(async () => {
+    await getStations({variables: {bounds: mapBounds(map.getBounds())}});
+  }, [getStations, map]);
+
+  useEffect(moveEnd, [moveEnd, map]);
+
+  useEffect(() => {
+    map.on('moveend', moveEnd);
+    return () => {
+      map.off('moveend', moveEnd);
+    };
+  }, [map, moveEnd]);
+
+  useEffect(() => {
+    if (data) setStations(data.stationsByBounds);
+  }, [data]);
+
+  const displayStations = useMemo(() => (
+      <>
+        {stations.map(s => (
+            <Marker key={s.id} position={[
+              s.geometry.coordinates[1],
+              s.geometry.coordinates[0]]}></Marker>
+        ))}
+      </>
+  ), [stations]);
+
   return (
-      <MapContainer style={{width, height}} center={center} zoom={13} minZoom={11} zoomControl={false}>
+      <div>
+        {displayStations}
+      </div>
+  );
+};
+
+const Map = ({
+               width = '100vw',
+               height = '100vh',
+               center = position,
+               showAllStation = false,
+             }) => {
+  const [map, setMap] = useState(null);
+  return (
+      <MapContainer style={{width, height}} center={center}
+                    zoom={13}
+                    minZoom={11}
+                    ref={setMap}
+                    zoomControl={false}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <ZoomControl position="bottomright" />
-        <MapEvents moveEndedAction={moveEndedAction}/>
-        {stations && stations.map(s => {
-          return <StationMarker  key={s.id} station={s} />
-        })}
+        <ZoomControl position="bottomright"/>
+        {map && showAllStation ?
+            <DisplayStations map={map}/> : null}
       </MapContainer>
   );
 };
