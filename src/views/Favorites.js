96d949b67597ffea2dd6a5c616d70ac81ae53d43
jopakka <jopakkamies@gmail.com>
2022-04-26 12:17:11 +0300

    favorites has loading indicator

--- src/views/Favorites.js
+++ src/views/Favorites.js
@@ -2,17 +2,38 @@ import {useQuery} from '@apollo/client';
 import {favorites} from '../utils/queries';
 import {useEffect, useState} from 'react';
 import FavList from '../components/FavList';
+import LoadingIndicator from '../components/LoadingIndicator';
 
 const Favorites = () => {
-  const {data, refetch} = useQuery(favorites, {fetchPolicy: 'network-only'});
+  const {data, refetch} = useQuery(favorites, {
+    fetchPolicy: 'network-only',
+    onCompleted: () => {
+      setLoading(false);
+    },
+    onError: () => {
+      setLoading(false);
+    },
+  });
   const [favs, setFavs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const doRefetch = async () => {
+    try {
+      setLoading(true)
+      await refetch();
+    } catch (e) {}
+  }
 
   useEffect(() => {
     if (data) setFavs(data.favorites);
   }, [data]);
   return (
       <>
-        {favs.map(f => <FavList key={f.stationID} item={f} refetch={refetch}/>)}
+        {loading ? <LoadingIndicator /> :
+            favs.length > 0 ?
+                favs.map(f => <FavList key={f.stationID} item={f} refetch={doRefetch}/>) :
+                <p>No favorites</p>
+        }s
       </>
   );
 };
