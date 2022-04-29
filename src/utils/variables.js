26d2d3029f6ca616827c861481ad64324b557491
jopakka <jopakkamies@gmail.com>
2022-04-29 11:40:03 +0300

    added basename when is production

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -2,7 +2,6 @@ const apiUrl = 'https://localhost:8000';
 // const apiUrl = 'https://192.168.1.11:8000';
 // const apiUrl = 'https://joonaun-project.azurewebsites.net';
 const graphqlUrl = `${apiUrl}/graphql`;
-// const basename = "/~joonaun/sssf/project/"
-const basename = "/"
+const basename = "/~joonaun/sssf/project/"
 
 export {apiUrl, graphqlUrl, basename};
